74d6fe1d787922a26d5ae06262384589ed20fcb4
John Doe <abdullahalmasrur8@gmail.com>
2024-07-07 22:21:16 +0600

    #

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,14 +3,6 @@ import { Display } from "./Display";
 
 export default function App() {
   return (
-    <div>
-      <Calculator />
-    </div>
-  );
-}
-
-function Calculator() {
-  return (
     <div className="calc">
       <Display />
       <ButtonList />
